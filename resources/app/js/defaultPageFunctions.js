ef6c3e2df95ab9c113005bcf5d2a4280d75ed597
Sargamesh <digradom@gmail.com>
2017-03-23 21:48:44 -0400

    +Added Json Reading +Added General settings and WIi U JSONS +Added a Test Bench and Page Template
    *Make forms that write to JSON for Setup page

---
+++ resources/app/js/defaultPageFunctions.js
@@ -0,0 +1,18 @@
+function InsertWindowControl() {
+document.getElementById('windowControl').innerhtml(
+  <!-- WINDOW CONTROL START-->
+
+    <div class="col-10">
+      <p>Super Console Version Number</p>
+    </div>
+    <div class="col-2">
+      <div id="title-bar-btns">
+             <button id="min-btn">-</button>
+             <button id="max-btn">+</button>
+             <button id="close-btn">x</button>
+        </div>
+    </div>
+  <!-- WINDOW CONTROL END-->
+);
+
+}
